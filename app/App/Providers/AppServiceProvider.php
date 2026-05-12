1b10b5eaecaab0e96d9424967a7b7ce0a5f862b7
Karina Ghaisani <karinaghsn@gmail.com>
2026-05-12 17:22:23 +0700

    Fix namespace issues in AppServiceProvider and add API routes for user management

--- app/App/Providers/AppServiceProvider.php
+++ app/App/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Domains\User\Interfaces\UserRepositoryInterface;
-use Domains\User\Repositories\UserRepository;
+use Domain\User\Interfaces\UserRepositoryInterface;
+use Domain\User\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users', [UserController::class, 'index']);
