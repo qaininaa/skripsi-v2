a7a6afbd71d26ff30de6198b00852a09759efa7a
Karina Ghaisani <karinaghsn@gmail.com>
2026-05-13 06:41:11 +0700

    feat: implement login form and routes for user authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +8,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+
+Route::get('/users', [UserController::class, 'index'])->middleware('role:super')->name('users.index');
+Route::get('/users/create', [UserController::class, 'create'])->middleware('role:super')->name('users.create');
+Route::post('/users/store', [UserController::class, 'store'])->middleware('role:super')->name('users.store');
