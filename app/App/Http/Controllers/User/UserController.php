b1c9d6a78019ab1c4d2cd8ec05b7f838eb299907
Karina Ghaisani <karinaghsn@gmail.com>
2026-05-12 18:10:53 +0700

    Fix view path in UserController index method to point to user-management

--- app/App/Http/Controllers/User/UserController.php
+++ app/App/Http/Controllers/User/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     public function index()
     {
         $users = $this->userService->getDataUsers();
-        return view('user.index', compact('users'));
+        return view('user-management.index', compact('users'));
     }
 
     public function getUsers()
