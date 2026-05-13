b1cdbf347dc999d31d60d00974b7b20a63ead5f8
Karina Ghaisani <karinaghsn@gmail.com>
2026-05-13 11:17:59 +0700

    feat: simplify home route redirection and update dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Dashboard\DashboardController;
 use App\Http\Controllers\User\UserController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    if (!Auth::check()) {
-        return redirect()->route('login');
-    }
+    return redirect()->route('login');
+});
 
-    return redirect()->route('welcome');
-})->name('home');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('dashboard');
 
 Route::get('/welcome', function () {
     return view('welcome');
