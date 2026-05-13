bcb25888f47232a5c7dbfd4b856479f7dcb02690
Karina Ghaisani <karinaghsn@gmail.com>
2026-05-13 17:35:44 +0700

    feat: reorganize routes to include initial password change functionality and adjust middleware for dashboard access

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\InitialPasswordController;
 use App\Http\Controllers\Dashboard\DashboardController;
 use App\Http\Controllers\User\PasswordSettingController;
 use App\Http\Controllers\User\UserController;
@@ -10,10 +11,6 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware(['auth'])
-    ->name('dashboard');
-
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.post');
@@ -21,12 +18,18 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/password/change-initial', [InitialPasswordController::class, 'edit'])->name('password.change.form');
+    Route::put('/password/change-initial', [InitialPasswordController::class, 'update'])->name('password.change.update');
 });
 
-Route::middleware(['auth', 'role:super'])->group(function () {
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-    Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
-    Route::get('/settings/password', [PasswordSettingController::class, 'index'])->name('settings.index');
-    Route::put('/settings/password', [PasswordSettingController::class, 'update'])->name('settings.update');
+Route::middleware(['auth', 'password.changed'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::middleware('role:super')->group(function () {
+        Route::get('/users', [UserController::class, 'index'])->name('users.index');
+        Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+        Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
+        Route::get('/settings/password', [PasswordSettingController::class, 'index'])->name('settings.index');
+        Route::put('/settings/password', [PasswordSettingController::class, 'update'])->name('settings.update');
+    });
 });
