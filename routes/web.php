4c60df27b823e84832ac7a51083845cb68ce297e
Karina Ghaisani <karinaghsn@gmail.com>
2026-05-13 11:20:32 +0700

    feat: remove welcome route and refactor user routes for better organization

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth'])
     ->name('dashboard');
 
-Route::get('/welcome', function () {
-    return view('welcome');
-})->middleware('auth')->name('welcome');
-
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.post');
@@ -26,6 +22,8 @@ Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
-Route::get('/users', [UserController::class, 'index'])->middleware(['auth', 'role:super'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->middleware(['auth', 'role:super'])->name('users.create');
-Route::post('/users/store', [UserController::class, 'store'])->middleware(['auth', 'role:super'])->name('users.store');
+Route::middleware(['auth', 'role:super'])->group(function () {
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
+});
