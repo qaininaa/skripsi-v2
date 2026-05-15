c2bb24acd8ed6c5284117e3de72be974c7374dab
Karina Ghaisani <karinaghsn@gmail.com>
2026-05-15 14:37:28 +0700

    feat: add template migrations for reports, media, and incubators

---
+++ database/migrations/2026_05_15_032259_create_report_templates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('report_templates', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->integer('annex_number');
+            $table->string('sop_code');
+            $table->string('sop_version');
+            $table->boolean('has_personnel')->default(false);
+            $table->unique(['annex_number', 'sop_code', 'sop_version']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('report_templates');
+    }
+};
